adecc99d3bdd242ccc650f3eeb1de8310196869a
Valya Kulakova <kulakova-vv@mail.ru>
2025-02-24 23:01:33 +0300

    My first commit

---
+++ lexicon/lexicon_ru.py
@@ -0,0 +1,6 @@
+LEXICON_RU: dict[str, str] = {
+    '/start': '<b>Привет!</b>',
+    '/help': 'ПОМОЩЬ',
+    'yes': 'Да',
+    'no': 'Нет'
+}
---
+++ main.py
@@ -0,0 +1,33 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from config_data.config import Config, load_config
+from handlers import other_handlers, user_handlers
+
+logger = logging.getLogger(__name__)
+
+# Функция конфигурирования и запуска бота
+async def main() -> None:
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+        '[%(asctime)s] - %(name)s -%(message)s')
+
+    logger.info('Starting bot') #начало работы бота
+
+    config: Config = load_config()
+
+    bot = Bot(token=config.tg_bot.token)
+    dp = Dispatcher()
+
+    # Регистриуем роутеры в диспетчере, порядок важен!
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    await bot.delete_webhook(drop_pending_updates=True) #пропускаем накопившие апдейты
+    await dp.start_polling(bot)                         #запускаем поллинг
+
+asyncio.run(main())
