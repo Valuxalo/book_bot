c5d2c5c504870f3fa904832ef2c141412b61550c
Valya Kulakova <kulakova-vv@mail.ru>
2025-03-06 22:05:51 +0300

    Add file set_menu

--- lexicon/lexicon_ru.py
+++ lexicon/lexicon_ru.py
@@ -4,3 +4,8 @@ LEXICON_RU: dict[str, str] = {
     'yes': 'Да',
     'no': 'Нет'
 }
+
+LEXICON_COMMANDS_RU: dict[str, str] = {
+    '/command_1': 'descr_command',
+    '/command_2': 'descr_command'
+}
